fdc860c3642ac4c41a189e1f013976e8a86b0578
quannm19 <quannm19@fsoft.com.vn>
2017-05-11 14:55:25 +0700

    x

---
+++ a.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("nhap vao 1 so a: ");
+scanf("%d",&a)
+printf("nhap vao 1 so b: ");
+scanf("%d",&b)
+printf("ban da nhap vao %d %d\n",a,b);
+return 0;
+}
+
